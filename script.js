034f3873a2f51e36a5b4e100b27921274e9506ee
marianalimoes <66639848+marianalimoes@users.noreply.github.com>
2020-10-28 21:28:47 +0000

    final

--- script.js
+++ script.js
@@ -1,94 +1,50 @@
 var d = document;
 
-//botão e div que sobe e desce (slider)
-var toggle = document.getElementById('toggle');
-var slider = document.querySelector('.slider');
-
-toggle.addEventListener('click', toggleSlider);
-
-// se o slider estiver aberto, muda o nome para closed
-// (e vice-versa)
+// OPEN & CLOSE
+// se o slider estiver aberto, muda o nome para .closed (e vice-versa)
 // no CSS altera-se a localização (-100%)
+var toggle = d.getElementById('toggle');
+var slider = d.querySelector('.slider');
+
+// no click, abrir
+toggle.addEventListener('click', toggleSlider);
 function toggleSlider(){
-    if (slider.classList.contains('closed')) {
-        slider.classList.remove('closed');
-        slider.classList.add('opened');
-    } else {
-        slider.classList.remove('opened');
-        slider.classList.add('closed');
-    }
+    slider.classList.toggle('opened');
 }
 
+// AUDIO
 // unlock audio quando se clica no toggle
 function playAudio(url) {
     new Audio(url).play();
 }
 
-
-/*obter altura dos .title-wrappers
-para centrar os h1 em altura*/
-const vh = screenHeight;
-var wrapper = document.getElementsByClassName('title-wrapper');
-
-// calc é 100% da altura do viewport,
-// menos altura do div
-// a dividir por dois para fazer a margin-top
-
-/*getElementsByClassName retorna uma array
-então temos de iterar todas as wrapper*/
- /*
-    // caso seja fora de loop
-    wrapper[0].style.marginTop = (vh - wrapper[0].offsetHeight) /2 +"px";
-
-    // margin top é 221
-    console.log(wrapper[0].style.marginTop,
-        "221px");
-
-    wrapper[1].style.marginTop = (vh - wrapper[1].offsetHeight) /2 +"px";  */
-
-for (var i = 0; i < wrapper.length; i++) {
-    wrapper[i].style.marginTop = (vh - wrapper[i].clientHeight) /2 +"px";
-
-    console.log("scroll", wrapper[0].scrollHeight);
-    console.log("client", wrapper[0].clientHeight);
- }
-
-
- // TRANSPARENCY
 // IP LOCATION
-/* apenas funciona dentro do protocolo
-de segurança https */
-
 // recorrer a este website para aceder ao IP do user
 fetch('https://ipapi.co/json/')
     .then(function(response) {
         response.json().then(jsonData => {
             console.log(jsonData.city);
             
-            d.getElementById("online").innerText = "Are you online? " + navigator.onLine;
-
+            // ir buscar respetivo dado e
+            // escrever dentro de cada elemento
+            d.getElementById('online').innerText = "Are you online? " + navigator.onLine;
+            d.getElementById("ip").innerText = "Your IP Address: " + jsonData.ip;
             d.getElementById("cont").innerText = "Continent: " + jsonData.continent_code;
-            d.getElementById("countryCode").innerText = "Country Code: " + jsonData.country_code;
-            d.getElementById("country").innerText = "Country: " + jsonData.country_name;
+            d.getElementById("country").innerText = "Country: " + jsonData.country_code + " | " + jsonData.country_name;
             d.getElementById("region").innerText = "Region: " + jsonData.region;
             d.getElementById("city").innerText = "City: " + jsonData.city;
             d.getElementById("postal").innerText = "Postal-Code: " + jsonData.postal;
-            d.getElementById("latlong").innerText = "Coordinates: " + jsonData.latlong;
+            d.getElementById("latlong").innerText = "Coordinates: " + jsonData.lat + ", " + jsonData.long;
             d.getElementById("timezone").innerText = "Timezone: " + jsonData.timezone;
             d.getElementById("utc").innerText = "UTC: " + jsonData.utc_offset;
             d.getElementById("calling").innerText = "Dial Code: " + jsonData.country_calling_code;
-            d.getElementById("currency").innerText = "Currency Code: " + jsonData.currency;
             d.getElementById("currencyName").innerText = "Currency: " + jsonData.currency_name;
             d.getElementById("lang").innerText = "Languages: " + jsonData.languages;
             d.getElementById("org").innerText = "Organization: " + jsonData.org;
             d.getElementById("ip").innerText = "Your IP Address: " + jsonData.ip;
-            
-            d.getElementById("appVersion").innerText = "App Version: " + navigator.appVersion;
-            d.getElementById("userAgent").innerText = "User Agent: " + navigator.userAgent;
-            d.getElementById("screenW").innerText = "Screen Width: " + screen.width;
-            d.getElementById("screenH").innerText = "Screen Height: " + screen.height;
-            d.getElementById("screenH").innerText = "Screen Height: " + screen.height;
             d.getElementById("platform").innerText = "Platform: " + navigator.platform;
+            d.getElementById("userAgent").innerText = "User Agent: " + navigator.userAgent;
+            d.getElementById("appVersion").innerText = "App Version: "+ navigator.appVersion;
             
         });
     })
@@ -96,14 +52,51 @@ fetch('https://ipapi.co/json/')
         console.log(error)
     });
 
+// FONT WEIGHT
+// após x milissegundos, aumentar tamanho de privacy
+setTimeout(weight,700)
+var priv = d.getElementById("privacy");
+function weight() {
+    priv.classList.toggle('black');
+}
 
- document.getElementById("demo").innerHTML =
- "Screen width: " + screen.width
- + ", Screen Height: " + screen.height
- + ", Screen Color Depth: " + screen.colorDepth
- + ", App Version: " + navigator.appVersion
- + ", User Agent:" + navigator.userAgent
- + ", Platform: " + navigator.platform
- + ", Language: " + navigator.platform
- + ", Are you online?" + navigator.onLine
- + ", Everything: " + jsonData;
+
+// SCROLLER
+setInterval(scroller, 10);
+
+// apanhar as linhas do texto (<p>)
+let lines = d.querySelectorAll('.line');
+
+// randomizar começo/left de cada line
+function getRandom(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function scroller() {
+    // como querySelectorAll retorna uma lista,
+    // usar um loop para iterar por todas as lines
+    lines.forEach(line => {
+
+        // aplicar left random para começar em sítios diferentes
+        ////// TENTATIVA de aplicar nrs randomizados à padding da linha
+        // var rando = getRandom(5)
+        // line.style.paddingLeft = rando;
+
+               // para aumentara margem da esquerda
+        let margin = line.offsetLeft;
+        margin += 1;
+
+        // aplicar margem
+        line.style.left= `${margin}px`;
+        // string literal; igual a escrever (""+margin+"px")
+
+        // para voltar ao início
+        const canvas = innerWidth;
+        const lineWidth = line.getBoundingClientRect().width;
+
+        // se margem da esq. for > que o viewport, voltar ao início
+        if (margin > canvas){
+            line.style.left = "" + lineWidth *(-1) + "px";
+        }
+    })
+}
